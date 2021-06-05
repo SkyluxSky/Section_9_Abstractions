251fa09e158929a3abce54b58b57fe0538098934
SkyluxSky <skyluxsky@gmail.com>
2021-06-05 16:08:51 -0400

    Interface Part 2

--- Interfaces/src/SkyluxSky/Main.java
+++ Interfaces/src/SkyluxSky/Main.java
@@ -10,10 +10,18 @@ public class Main {
 
         //How to implement interfaces in main method - Do not type new interface, only call the class that implements the interface.
         ITelephone camsPhone;
-        camsPhone = new DeskPhone(12325);
 
+        //Desk Phone
+        camsPhone = new DeskPhone(12325);
         camsPhone.powerOn();
         camsPhone.callPhone(12325);
+        camsPhone.answer();
+
+        //Because our initial object is initialized as ITelephone Interface
+        //We can create a single instance that stores multiple objects that use the same interface.
+        camsPhone = new MobilePhone(23456);
+        camsPhone.powerOn();
+        camsPhone.callPhone(23456);
         camsPhone.answer();
     }
 }
