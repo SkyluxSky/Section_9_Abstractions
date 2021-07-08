7681c98f6455a369addab699b43067733e7819e1
SkyluxSky <skyluxsky@gmail.com>
2021-07-08 14:20:38 -0400

    Created Abstract_Class.java

---
+++ Abstract_Classes/src/SkyluxSky/Main.java
@@ -0,0 +1,10 @@
+/*Abstraction is when you describe the functionality of something without going into the details.*/
+
+package SkyluxSky;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
